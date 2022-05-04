df2c7bffbfa72248f0387e81cd853453bb5d0366
rebbitR <renakdev@gmail.com>
2022-05-04 14:47:35 +0300

    check time
    check the time of frame at the models

--- main.py
+++ main.py
@@ -3,21 +3,49 @@ import classes
 from log import getLog,send_buf_to_log
 from cut_video import cut_video_by_second,videotoframes,framestovideo
 
-import time
-
 import cv2
+
+from datetime import datetime
 
 
 def main(path):
-    t1=time.time()
-    buf=cut_video_by_second(path)
-    # buf1=videotoframes(path)
-    # buf=[]
-    # for i in range(len(buf1)-1):
-    #     frame=classes.frame(buf1[i],i)
-    #     buf.append(frame)
-    #     # print(type(buf[i]))
-    # print(len(buf))
+
+    # buf=cut_video_by_second(path)
+    buf1=videotoframes(path)
+    buf=[]
+    for i in range(len(buf1)-1):
+        frame=classes.frame(buf1[i],i)
+        buf.append(frame)
+        # print(type(buf[i]))
+    print(len(buf))
+
+    # now = datetime.now()
+    #
+    # current_time = now.strftime("%H:%M:%S")
+    # print("Current Time =", current_time)
+    # buf[111].yolo_detect()
+    #
+    # # cut_objects_from_frame():
+    # buf[111].cut_objects()
+    # # find_kinds_with_model:
+    # buf[111].model('resnet_50')
+    # # buf[111].model('binary_vgg16')
+    # # buf[111].model('category_vgg16')
+    # buf[111].result()
+    # # now = datetime.now()
+    # #
+    # # current_time = now.strftime("%H:%M:%S")
+    # # print("Current Time =", current_time)
+    # buf[111].print_results_frame()
+    # buf[111].tryAddTxt()
+    # now = datetime.now()
+    #
+    # current_time = now.strftime("%H:%M:%S")
+    # print("Current Time =", current_time)
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
     for frame1 in buf:
         # find_objects at frame _with_yolo:
         frame1.yolo_detect()
@@ -26,28 +54,28 @@ def main(path):
         frame1.cut_objects()
         # find_kinds_with_model:
         frame1.model('resnet_50')
-        frame1.model('binary_vgg16')
-        frame1.model('category_vgg16')
+        # frame1.model('binary_vgg16')
+        # frame1.model('category_vgg16')
         frame1.result()
         frame1.print_results_frame()
         frame1.tryAddTxt()
         # frame1.show_img()
-        # if len(frame1.objectsC)!=0:
-        #     for my_obg in frame1.objectsC:
-        #         print(my_obg.models)
-        # frame1.show_img(txt='its worked!!!',waitKey=0)
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
 
 
 
     # send_buf_to_log(buf)
     # log = getLog()
 
-    t2=time.time()
-    print('time s: ',t1-t2)
+
 
     return buf
 
-# buf = program("video/V_AIRPLANE_038.mp4")
+
+# buf = main("video/V_DRONE_001.mp4")
 # for i in buf:
 #     i.show_img()
 
