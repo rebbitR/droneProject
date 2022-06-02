58680543ae849818ccc1db225a14c08e97c8d55c
rebbitR <renakdev@gmail.com>
2022-06-02 03:02:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,109 +1,71 @@
-# main number 2 try option 2 with classes_try:---------
-import threading
 
 import classes
-from log import getLog,send_buf_to_log
-from cut_video import cut_video_by_second,videotoframes,framestovideo
-from load_model import my_model
-import cv2
-from multiprocessing import Pool
-
+from log import write_to_log
+from video import video_to_frames,frames_to_video
+# from multiprocessing import Pool
+# import threading
 from datetime import datetime
 
+def print_current_time():
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    write_to_log(current_time)
 
 def main(path):
 
-    # buf=cut_video_by_second(path)
-    buf1=videotoframes(path)
+    buf1=video_to_frames(path)
     buf=[]
     for i in range(len(buf1)-1):
-        frame=classes.frame(buf1[i],i)
+        frame=classes.Frame(buf1[i],i)
         buf.append(frame)
-        # print(type(buf[i]))
-    print(len(buf))
+    write_to_log(str(len(buf))+" frames---")
 
-    # now = datetime.now()
-    #
-    # current_time = now.strftime("%H:%M:%S")
-    # print("Current Time =", current_time)
-    # buf[111].yolo_detect()
-    #
-    # # cut_objects_from_frame():
-    # buf[111].cut_objects()
-    # # find_kinds_with_model:
-    # buf[111].model('resnet_50')
-    # # buf[111].model('binary_vgg16')
-    # # buf[111].model('category_vgg16')
-    # buf[111].result()
-    # # now = datetime.now()
-    # #
-    # # current_time = now.strftime("%H:%M:%S")
-    # # print("Current Time =", current_time)
-    # buf[111].print_results_frame()
-    # buf[111].tryAddTxt()
-    # now = datetime.now()
-    #
-    # current_time = now.strftime("%H:%M:%S")
-    # print("Current Time =", current_time)
-    now = datetime.now()
-
-    current_time = now.strftime("%H:%M:%S")
-    print("Current Time =", current_time)
-    for frame1 in buf:
+    print_current_time()
+    for frame in buf:
         # find_objects at frame _with_yolo:
-        frame1.yolo_detect()
+        frame.detect_objects()
 
         # cut_objects_from_frame():
-        frame1.cut_objects()
+        frame.cut_objects()
+
         # find_kinds_with_model:
+
         # try multiprocessing
-        # t=[]
-        # arr=['resnet_50', 'binary_vgg16']
-        # for i in range(2):
-        #     t.append(threading.Thread(target=frame1.model,args=(arr[i],)))
+        # t = []
+        # arr = ['resnet_50', 'binary_vgg16', 'category_vgg16']
+        # for i in range(3):
+        #     t.append(threading.Thread(target=frame.model_detect, args=(arr[i],)))
         #     t[i].start()
-        # for i in range(2):
+        # for i in range(3):
         #     t[i].join()
-        t = []
-        arr = ['resnet_50', 'binary_vgg16', 'category_vgg16']
-        for i in range(3):
-            t.append(threading.Thread(target=my_model, args=('object0.png', 81, arr[i])))
-            t[i].start()
-        for i in range(3):
-            t[i].join()
-        # with Pool(3) as p:
-        #     p.map(frame1.model, ['resnet_50', 'binary_vgg16', 'category_vgg16'])
-        # frame1.model('resnet_50')
-        # frame1.model('binary_vgg16')
-        # frame1.model('category_vgg16')
-        frame1.result()
-        frame1.print_results_frame()
-        frame1.tryAddTxt()
+
+        print_current_time()
+        # frame.model_detect('resnet_50')
+        # frame.model_detect('binary_vgg16')
+        frame.model_detect('category_vgg16')
+
+        frame.result()
+        frame.print_results_frame()
+        print_current_time()
+        frame.add_txt()
         # frame1.show_img()
-    now = datetime.now()
-
-    current_time = now.strftime("%H:%M:%S")
-    print("Current Time =", current_time)
+        buf[frame.idC]=frame.frameC
+    print_current_time()
 
 
 
-    # send_buf_to_log(buf)
-    # log = getLog()
 
-
+    # buf1 = []
+    # for i in buf:
+    #     buf1.append(i.frameC)
 
     return buf
 
 if __name__ == '__main__':
 
-    buf = main("video/V_AIRPLANE_048.mp4")
-# for i in buf:
-#     i.show_img()
-
-# buf1=[]
-# for i in buf:
-#     buf1.append(i.frameC)
-# framestovideo("airplane-res.mp4",buf1)
+    buf = main("video/V_AIRPLANE_042.mp4")
+    frames_to_video("airplane-res2.mp4",buf)
 
 
 
