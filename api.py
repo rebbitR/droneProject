e33181ed4814d86fe5b34598962a21a94ef24b1a
rebbitR <renakdev@gmail.com>
2022-06-07 01:27:05 +0300

    Update api.py

--- api.py
+++ api.py
@@ -2,14 +2,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from main import main
-from video import framestovideo
+from video import frames_to_video
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/post', methods=['POST'])
 def program():
-
+    print('i am in program')
     # video_path="D:/react/src/video/myvideo_1.mp4"
     # res_video_path='D:/react/src/video/videoneww_1.mp4'
     video_path=r'C:\Users\רננה קייקוב\Desktop\Project\droneProject-client\react\src\video\myvideo_1.mp4'
@@ -19,11 +19,7 @@ def program():
     file.save(video_path)
 
     buf=main(video_path)
-
-    buf1=[]
-    for i in buf:
-        buf1.append(i.frameC)
-    framestovideo(res_video_path,buf1)
+    frames_to_video(res_video_path,buf)
 
     filename1="videoneww_1.mp4"
     url1=res_video_path
@@ -34,6 +30,7 @@ def program():
 if __name__ == '__main__':
     # run app in debug mode on port 5000
     app.run(debug=True, port=5000)
+
 
 
 
