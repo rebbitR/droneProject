cce6d22d1dfc3a04c280de3af9843d26bb218f4c
rebbitR <renakdev@gmail.com>
2022-06-22 13:27:22 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,21 +1,18 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 from main import main
 from video import frames_to_video
-from log import CreateLog
+from log import create_log
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/post', methods=['POST'])
 def program():
-    print('i am in program')
-    CreateLog()
-    video_path="D:/react/src/video/myvideo_1.mp4"
+    create_log()
+    video_path="video_from_client/myvideo_1.mp4"
     res_video_path='videoNew_1.mp4'
-    # video_path=r'C:\Users\רננה קייקוב\Desktop\Project\droneProject-client\react\src\video\myvideo_1.mp4'
-    # res_video_path=r'C:\Users\רננה קייקוב\Desktop\Project\droneProject-client\react\src\video\videoneww_1.mp4'
 
     file = request.files['myFile']
     file.save(video_path)
@@ -23,12 +20,8 @@ def program():
     buf=main(video_path)
     frames_to_video(res_video_path,buf)
 
-    filename1="videoneww_1.mp4"
     return 'http://127.0.0.1:8887/'+'videoNew_1.mp4'
-    # url1=res_video_path
-    # describe=['detection drone']
-    #
-    # return jsonify({'filename': filename1,'url':url1,'dis':describe})
+
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
